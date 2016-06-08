27d55c4561f3205c1b30bc38c4af55c9bdf17832
choijaewoong <jwchoi0129@gmail.com>
2016-06-08 21:38:29 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var morgan = require('morgan');
+var express = require('express');
+var bodyParser = require('body-parser'); // post 요청을 위한 body-parser
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(morgan('dev'));
+app.use(handleError);
+
+app.listen(3000);
+
+function handleError(err, req, res, next) {
+   res.status(err.code).send({msg:err.message});
+}
