7a75b1a6d714bb67167ee91b88d83645a515bbea
choijaewoong <jwchoi0129@gmail.com>
2016-06-08 22:45:16 +0900

    (router) 앱 추가 메소드 구현

--- appRouter.js
+++ appRouter.js
@@ -2,10 +2,12 @@ var express = require('express');
 var App = require('./appModel');
 var router = express.Router();
 
-router.get('/apps', showAppList);
+router.get('/applist', showAppList);
+router.post('/addapp', addApp);
 
+// 모든 앱 리스트 출력 메소드
 function showAppList(req, res, next) {
-   App.find({},{_id:1, name:1}).then(function fulfilled(docs) {
+   App.find({},{_id:1, name:1, image:1}).then(function fulfilled(docs) {
       console.log('Success : ');
       var result = {
          count : docs.length,
@@ -15,6 +17,23 @@ function showAppList(req, res, next) {
    }, function rejected(err) {
       err.code = 500;
       next(err);
+   });
+}
+
+// 앱 추가 메소드
+function addApp(req, res, next) {
+   var name = req.body.name;
+   var image = req.body.image;
+   var package_name = req.body.package_name;
+   var activity_name = req.body.activity_name;
+   
+   var app = new App({name:name, image:image, package_name:package_name, activity_name:activity_name});
+   app.save().then(function fulfilled(result){
+      console.log(result);
+      res.send({msg:'success', id:result._id});
+   }, function rejected(err) {
+      err.code = 500;
+      next(err);      
    });
 }
 
