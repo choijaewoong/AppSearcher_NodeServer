8416fd1b4f40009469a3cf4305c198a6f08d3922
choijaewoong <jwchoi0129@gmail.com>
2016-06-08 21:58:21 +0900

    app 모델 스키마 구현

---
+++ appModel.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var url = 'mongodb://52.78.29.249:27017/applist';
+mongoose.connect(url);
+var conn = mongoose.connection;
+
+conn.on('error', function(err) {
+   console.error('Error : ', err);
+});
+
+conn.on('open', function() {
+   console.log('Connect');
+});
+
+// 앱 저장 스키마
+var AppSchema = mongoose.Schema({
+   name : String,
+   image : String,
+   package_name : String,
+   activity_name : String   
+});
+
+var App = mongoose.model('App', AppSchema);
+
+module.exports = App;
