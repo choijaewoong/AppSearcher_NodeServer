d685db72dc3b04b45eb355656255d54a89d4fb44
choijaewoong <jwchoi0129@gmail.com>
2016-06-14 19:05:52 +0900

    게시글 작성 요청 기능 구현

--- router/postRouter.js
+++ router/postRouter.js
@@ -13,7 +13,7 @@ router.use(passport.initialize());
 router.use(passport.session());
 
 router.get('/postlist', showPostList);
-// router.post('/writepost', writePost);
+router.post('/writepost', writePost);
 
 // 모든 게시글 리스트 출력 메소드
 function showPostList(req, res, next) {
@@ -31,6 +31,31 @@ function showPostList(req, res, next) {
         }, function rejected(err) {
             err.code = 500;
             next(err);
+        });
+   }
+}
+
+// 게시글 추가 메소드
+function writePost(req, res, next) { 
+   var user = req.user.user;
+   if(!user){
+       res.sendStatus(401);
+   }else{ 
+        var app_name = req.body.app_name;
+        var app_evaluation = req.body.app_evaluation;
+        var app_image = req.body.app_image;
+        var write_date = req.body.write_date;
+        var post = new Post({app_name:app_name, 
+                           user_name:user.name, 
+                           app_evaluation:app_evaluation, 
+                           app_image:app_image,
+                           write_date:write_date});
+        post.save().then(function fulfilled(result){
+            console.log(result);
+            res.send({ post : result});
+        }, function rejected(err) {
+            err.code = 500;
+            next(err);      
         });
    }
 }
